e5fe8e3cf735053c5099393458442468e1ae499f
Mistire <96515111+Mistire@users.noreply.github.com>
2024-01-24 10:04:52 +0300

    Time: 39 ms (62.55%), Space: 16.4 MB (63.12%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -6,17 +6,17 @@ class Solution:
         l,r = 0, len(nums) - 1
         i = 0
         
-        def swap(i, j):
-            tmp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = tmp
+        # def swap(i, j):
+        #     tmp = nums[i]
+        #     nums[i] = nums[j]
+        #     nums[j] = tmp
         
         while i <= r:
             if nums[i] == 0:
-                swap(l,i)
+                nums[l], nums[i] = nums[i], nums[l]
                 l += 1
             elif nums[i] == 2:
-                swap(i,r)
+                nums[i], nums[r] = nums[r], nums[i]
                 r -= 1
                 i -= 1
             i += 1
