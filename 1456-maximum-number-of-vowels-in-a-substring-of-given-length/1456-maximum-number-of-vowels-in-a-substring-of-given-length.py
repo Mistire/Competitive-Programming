49c4cd20242fe36edce071172b46333e12e48fcc
Mistire <96515111+Mistire@users.noreply.github.com>
2024-03-04 01:46:56 +0300

    Time: 101 ms (87.01%), Space: 17.3 MB (75.51%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,19 +1,44 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        vowels = set("aeiou")
-        maxVowels = 0
-        curVowels = 0
+        # vowels = set("aeiou")
+        # maxVowels = 0
+        # curVowels = 0
         
+        # for i in range(k):
+        #     if s[i] in vowels:
+        #         curVowels += 1
+        # maxVowels = curVowels
+        
+        # for i in range(k, len(s)):
+        #     if s[i] in vowels:
+        #         curVowels += 1
+        #     if s[i - k] in vowels:
+        #         curVowels -= 1
+        #     maxVowels = max(maxVowels, curVowels)
+        # return maxVowels
+
+        maxVow = 0
+        curVow = 0
+        vowels = set("aioue")
+
         for i in range(k):
             if s[i] in vowels:
-                curVowels += 1
-        maxVowels = curVowels
-        
+                curVow += 1
+        maxVow = curVow
+
         for i in range(k, len(s)):
             if s[i] in vowels:
-                curVowels += 1
-            if s[i - k] in vowels:
-                curVowels -= 1
-            maxVowels = max(maxVowels, curVowels)
-        return maxVowels
+                curVow += 1
+            if s[i-k] in vowels:
+                curVow -= 1
+            maxVow = max(curVow, maxVow)
+        return maxVow
+
+
+
+
                 
+
+
+
+
