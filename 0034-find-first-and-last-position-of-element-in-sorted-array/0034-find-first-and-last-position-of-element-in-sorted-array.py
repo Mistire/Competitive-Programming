a3cd3ccd58b47dcaf33d65b91f77489593b90c72
Mistire <96515111+Mistire@users.noreply.github.com>
2024-03-07 06:15:19 +0300

    Time: 80 ms (31.43%), Space: 18 MB (29.47%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,32 +1,32 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def binarySearchLeft(num, target):
-            lo, hi = 0, len(nums) - 1
+        # def binarySearchLeft(num, target):
+        #     lo, hi = 0, len(nums) - 1
 
-            while lo <= hi:
-                mid = (lo+hi) // 2
-                if nums[mid] < target:
-                    lo = mid + 1
-                else:
-                    hi = mid - 1
-            return lo
+        #     while lo <= hi:
+        #         mid = (lo+hi) // 2
+        #         if nums[mid] < target:
+        #             lo = mid + 1
+        #         else:
+        #             hi = mid - 1
+        #     return lo
 
-        def binarySearchRight(num, target):
-            lo, hi = 0, len(nums) - 1
+        # def binarySearchRight(num, target):
+        #     lo, hi = 0, len(nums) - 1
 
-            while lo <= hi:
-                mid = (lo+hi) // 2
-                if nums[mid] <= target:
-                    lo = mid + 1
-                else:
-                    hi = mid - 1
-            return hi
+        #     while lo <= hi:
+        #         mid = (lo+hi) // 2
+        #         if nums[mid] <= target:
+        #             lo = mid + 1
+        #         else:
+        #             hi = mid - 1
+        #     return hi
 
-        left = binarySearchLeft(nums,target)
-        right = binarySearchRight(nums, target)
+        left = bisect.bisect_left(nums,target)
+        right = bisect.bisect_right(nums, target)
 
-        if left <= right:
-            return [left, right]
+        if left < len(nums) and nums[left] == target:
+            return [left, right-1]
         else:
             return [-1, -1]
 
