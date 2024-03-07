052353f6c5192aba7b7b2cb3c79d7e63ff262154
Mistire <96515111+Mistire@users.noreply.github.com>
2024-03-07 04:01:04 +0300

    Time: 30 ms (95.37%), Space: 16.3 MB (99.88%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -4,7 +4,7 @@ class Solution:
         res = 0
         
         while l <= r:
-            m = l + (r - l) // 2
+            m = (l+r) // 2
             if m**2 > x:
                 r = m-1
             elif m**2 < x:
