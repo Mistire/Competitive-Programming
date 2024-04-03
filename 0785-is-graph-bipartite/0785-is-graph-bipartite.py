87308873946ed9521c610837b4289f523cad707c
Mistire <96515111+Mistire@users.noreply.github.com>
2024-04-03 19:37:58 +0300

    Time: 148 ms (47.35%), Space: 17.5 MB (7.65%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,21 +1,49 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        odd = [0] * len(graph)
-        def bfs(i):
-            if odd[i]:
-                return True
-            q = deque([i])
-            odd[i] = -1
-            while q:
-                i = q.popleft()
-                for nei in graph[i]:
-                    if odd[i] == odd[nei]:
+        color = [-1]* len(graph)
+        
+        def dfs(node):
+            for neighbours in graph[node]:
+                if color[neighbours] == -1:
+                    if color[node] == 0:
+                        color[neighbours] = 1
+                    else:
+                        color[neighbours] = 0
+                    if not dfs(neighbours):
                         return False
-                    elif not odd[nei]:
-                        q.append(nei)
-                        odd[nei] = -1 * odd[i]
+                else:
+                    if color[node] == color[neighbours]:
+                        return False
             return True
-        for i in range(len(graph)):
-            if not bfs(i):
-                return False
+
+
+        for node in range(len(graph)):
+            if color[node] == -1:
+                color[node] = 0
+                if not dfs(node):
+                    return False
         return True
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
