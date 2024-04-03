8b883151265553a27464697988b2fa4fd6eeebf2
Mistire <96515111+Mistire@users.noreply.github.com>
2024-04-03 03:31:16 +0300

    Time: 1679 ms (45.41%), Space: 143.7 MB (8.21%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,25 +1,70 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        adjList = defaultdict(list)
+        # adjList = defaultdict(list)
         
-        for node1, node2 in edges:
-            adjList[node1].append(node2)
-            adjList[node2].append(node1)
+        # for node1, node2 in edges:
+        #     adjList[node1].append(node2)
+        #     adjList[node2].append(node1)
             
-        visited = set()
+        # visited = set()
         
-        def dfs(node):
+        # def dfs(node):
+        #     if node == destination:
+        #         return True
+        #     if node not in visited:
+        #         visited.add(node)
+                
+        #         for neighbour in adjList[node]:
+        #             result = dfs(neighbour)
+        #             if result:
+        #                 return True
+                
+        #         # Add this return statement for the case when destination is not reached
+        #         return False
+                    
+        # return dfs(source)
+        def dfs(node, visisted):
             if node == destination:
                 return True
-            if node not in visited:
-                visited.add(node)
-                
-                for neighbour in adjList[node]:
-                    result = dfs(neighbour)
-                    if result:
+            visited.add(node)
+            for neighbours in graph[node]:
+                if neighbours not in visited:
+                    found = dfs(neighbours, visited)
+                    if found:
                         return True
-                
-                # Add this return statement for the case when destination is not reached
-                return False
-                    
-        return dfs(source)
+            return False
+
+        graph = defaultdict(list)
+
+        for node1, node2 in edges:
+            graph[node1].append(node2)
+            graph[node2].append(node1)
+
+        visited = set()
+        return dfs(source, visited)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
