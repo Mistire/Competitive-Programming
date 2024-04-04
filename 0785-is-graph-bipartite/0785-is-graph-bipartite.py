02928fbb5e261bb34c78c57d3b3bc99283f588e2
Mistire <96515111+Mistire@users.noreply.github.com>
2024-04-04 08:07:29 +0300

    Time: 145 ms (63.47%), Space: 17.1 MB (35.35%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -2,27 +2,43 @@ class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         color = [-1]* len(graph)
         
-        def dfs(node):
-            for neighbours in graph[node]:
-                if color[neighbours] == -1:
-                    if color[node] == 0:
-                        color[neighbours] = 1
-                    else:
-                        color[neighbours] = 0
-                    if not dfs(neighbours):
-                        return False
-                else:
-                    if color[node] == color[neighbours]:
-                        return False
-            return True
+        # def dfs(node):
+        #     for neighbours in graph[node]:
+        #         if color[neighbours] == -1:
+        #             if color[node] == 0:
+        #                 color[neighbours] = 1
+        #             else:
+        #                 color[neighbours] = 0
+        #             if not dfs(neighbours):
+        #                 return False
+        #         else:
+        #             if color[node] == color[neighbours]:
+        #                 return False
+        #     return True
 
+        # for node in range(len(graph)):
+        #     if color[node] == -1:
+        #         color[node] = 0
+        #         if not dfs(node):
+        #             return False
+        # return True
 
         for node in range(len(graph)):
             if color[node] == -1:
+                stack = [node]
                 color[node] = 0
-                if not dfs(node):
-                    return False
+
+            while stack:
+                node = stack.pop()
+
+                for neighbours in graph[node]:
+                    if color[neighbours] == -1:
+                        color[neighbours] = abs(color[node] - 1)
+                        stack.append(neighbours)
+                    elif color[node] == color[neighbours]:
+                        return False
         return True
+
             
 
 
