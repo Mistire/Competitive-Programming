81a44786d0d580343b057395c08523e8c12f8b15
Mistire <96515111+Mistire@users.noreply.github.com>
2024-04-15 14:43:48 +0300

    Time: 30 ms (95.92%), Space: 17 MB (31.46%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -7,9 +7,35 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        parentMap = {}
         def dfs(node, parent=None):
             if not node:
                 return
-            node.parent = parent
+            parentMap[node] = parent
             dfs(node.left, node)
             dfs(node.right, node)
+
+        dfs(root)
+
+        vis = set()
+        queue = deque([(target, 0)])
+        res = []
+
+        while queue:
+            node, distance = queue.popleft()
+
+            if node in vis:
+                continue
+            vis.add(node)
+            # print(vis)
+            if distance == k:
+                res.append(node.val)
+            elif distance < k:
+                for nei in (node.left, node.right, parentMap[node]):
+                    if nei and nei not in vis:
+                        queue.append((nei, distance+1))
+        
+        return res
+
+
+        
