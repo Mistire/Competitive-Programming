ad95df370e741773af007fc821815043ff846600
Mistire <96515111+Mistire@users.noreply.github.com>
2024-05-14 09:37:29 +0300

    Time: 1473 ms (5.05%), Space: 287.3 MB (11.3%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1
-        maxP = 0
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                maxP = max(maxP, profit)
-            else:
-                l = r
-            r += 1
-        return maxP
+        memo = {}
+
+        def dp(day, buy_stock):
+            if day >= len(prices):
+                return 0
             
-        
+            if (day, buy_stock) not in memo:
+                if buy_stock:
+                    memo[(day, buy_stock)] = max(prices[day], dp(day + 1, buy_stock))
+                else:
+                    memo[(day, buy_stock)] = max(dp(day + 1, not buy_stock) - prices[day], dp(day + 1, buy_stock))
+            
+            return memo[(day, buy_stock)]
+
+
+        return dp(0, False)
