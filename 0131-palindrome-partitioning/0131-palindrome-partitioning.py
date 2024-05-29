f9af0990cb2276645ad2c91249533167adc5aa6e
Mistire <96515111+Mistire@users.noreply.github.com>
2024-05-29 19:04:54 +0300

    Time: 522 ms (10.33%), Space: 32.2 MB (80.43%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,24 +1,22 @@
 class Solution:
-    
     def partition(self, s: str) -> List[List[str]]:
-        res, part = [], []
-
-        def dfs(i):
-            if i >= len(s):
-                res.append(part.copy())
-                return
-            for j in range(i, len(s)):
-                if isPali(s, i, j):
-                    part.append(s[i : j + 1])
-                    dfs(j + 1)
-                    part.pop()
-
-        def isPali(s, l, r):
-            while l < r:
-                if s[l] != s[r]:
-                    return False
-                l, r = l + 1, r - 1
-            return True
-        dfs(0)
-        return res
-
+        memo = {}
+        n = len(s)
+        def pali(strs):
+            if strs == strs[::-1]: 
+                return True 
+            else: 
+                return False
+        def dp(i):
+            ans = []
+            if i == n:
+                return [[]]
+            
+            for j in range(i, n):
+                prefix = s[i:j+1]
+                if pali(prefix):
+                    all_poss = dp(j+1)
+                    for poss in all_poss:
+                        ans.append([prefix]+poss)
+            return ans
+        return dp(0)
