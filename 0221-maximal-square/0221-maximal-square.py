e2019730491eaa8ff84cdc18b308d627b8efaa4c
Mistire <96515111+Mistire@users.noreply.github.com>
2024-06-20 23:51:36 +0300

    Time: 697 ms (18.83%), Space: 62 MB (9.95%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,20 +1,26 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        max_ones = 0
-        row = len(matrix)
-        col = len(matrix[0])
-        if matrix.count("1") >= col * row:
-            return 1
+        rows = len(matrix)
+        cols = len(matrix[0])
+        memo = {}
 
-        for i in range(row):
-            row_ones = 0
-            for j in range(col):
-                if matrix[i][j] == "1":
-                    row_ones += 1
-            max_ones = max(max_ones, row_ones)
-            temp = sqrt(max_ones)
-            if temp == (temp//1):
-                return max_ones
+        def dp(r, c):
+            if r >= rows or c >= cols:
+                return 0
 
-        return 1
+            if (r,c) not in memo:
+                down = dp(r+1, c)
+                right = dp(r, c+1)
+                diag = dp(r+1, c+1)
+
+                memo[(r, c)] = 0
+                if matrix[r][c] == '1':
+                    memo[(r, c)] = 1 + min(down, right, diag)
+            return memo[(r, c)]
+
+        dp(0, 0)
+        return max(memo.values()) ** 2
+                    
+        
+        
         
