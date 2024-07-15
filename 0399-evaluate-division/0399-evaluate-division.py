470ee7ce35b890e902386e82a89b337d231c9318
Mistire <96515111+Mistire@users.noreply.github.com>
2024-07-16 00:07:29 +0300

    Time: 48 ms (5.67%), Space: 16.6 MB (35.5%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,6 +1,6 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        adj = collections.defaultdict(list)
+        adj = defaultdict(list)
         for i, eq in enumerate(equations):
             a, b = eq
             adj[a].append([b,values[i]])
