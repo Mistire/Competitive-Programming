4b45dd818501d10a1840a7a217b72977084d350a
Mistire <96515111+Mistire@users.noreply.github.com>
2024-07-25 23:06:44 +0300

    Time: 49 ms (76.97%), Space: 16.5 MB (33.29%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,6 +1,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if str(x) == str(x)[::-1]:
-            return True
+        if str(x) != str(x)[::-1]:
+            return False
+        return True
             
             
