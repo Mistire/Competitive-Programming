e43981c92d70813a1041b88d0f266e298bd600ea
Mistire <96515111+Mistire@users.noreply.github.com>
2024-08-22 23:48:31 +0300

    Time: 38 ms (31.01%), Space: 16.5 MB (14.53%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,11 +1,10 @@
 class Solution:
     def findComplement(self, num: int) -> int:
         mask = 0
-        comp = ~num
+        compliment = ~num
 
-        while num > 0:
-            mask = (mask << 1) | 1
-            num >>= 1
-        res = comp & mask
+        for _ in range(num.bit_length()):
+            mask = (mask << 1) | 1   
+        res = compliment & mask
         return res
             
