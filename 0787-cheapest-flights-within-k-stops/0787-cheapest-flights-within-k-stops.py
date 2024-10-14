8a361ef327dd2d5085fcbd1f67edc830407032b5
Mistire <96515111+Mistire@users.noreply.github.com>
2024-10-14 16:32:59 +0300

    Time: 243 ms (6.21%), Space: 17.8 MB (62.54%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,23 +1,13 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-         # Step 1: Initialize the distance array
-        dist = [float('inf')] * n
-        dist[src] = 0  # The cost to reach the source city is 0
-        
-        # A temporary distance array to store results for the current iteration
-        temp_dist = dist[:]
-        
-        # Step 2: Perform K+1 relaxations (because K stops means K+1 edges)
-        for i in range(k + 1):
-            # Iterate over all the flights (edges)
+        dist_arr = [float('inf')] * n
+        dist_arr[src] = 0
+
+        temp_arr = dist_arr[:]
+
+        for i in range(k+1):
             for u, v, w in flights:
-                # If the source city has been reached
-                if dist[u] != float('inf'):
-                    # Relax the edge if a cheaper price is found
-                    temp_dist[v] = min(temp_dist[v], dist[u] + w)
-            
-            # Update the main distance array with this iteration's results
-            dist = temp_dist[:]
-        
-        # Step 3: Check if we found a valid path to the destination
-        return dist[dst] if dist[dst] != float('inf') else -1
+                if dist_arr[u] != float('inf'):
+                    temp_arr[v] = min(temp_arr[v], dist_arr[u] + w)
+            dist_arr = temp_arr[:]
+        return dist_arr[dst] if dist_arr[dst] != float('inf') else -1
