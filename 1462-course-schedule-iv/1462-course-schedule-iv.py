76246e2a2683e7560f6624323f4bcee17cd7a922
Mistire <96515111+Mistire@users.noreply.github.com>
2024-10-14 16:00:06 +0300

    Time: 1122 ms (23.47%), Space: 20.3 MB (43.05%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,33 +1,17 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        preq = [[] * numCourses for _ in range(numCourses)]
-        for pre, course in prerequisites:
-            preq[pre].append(course)
-
         reach = [[False] * numCourses for _ in range(numCourses)]
-        res = []
 
-        for i in range(numCourses):
-            queue = deque([i])
-            visit = set([i])
+        for u, v in prerequisites:
+            reach[u][v] = True
+        
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    reach[i][j] = reach[i][j] or (reach[i][k] and reach[k][j])
 
-            while queue:
-                node = queue.popleft()
-                
-                for nei in preq[node]:
-                    if nei not in visit:
-                        visit.add(nei)
-                        queue.append(nei)
-                        reach[node][nei] = True
         res = []
         for u, v in queries:
-            res.append(reach[u][v])
-
+            res.append(reach[u][v])               
+        
         return res
-
-        
-
-        
-
-        
-        
