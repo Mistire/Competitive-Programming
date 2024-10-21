85e40426ae4ee802d146bd1608d59f4cef3b6ec1
Mistire <96515111+Mistire@users.noreply.github.com>
2024-10-21 18:34:00 +0300

    Time: 34 ms (62.45%), Space: 16.3 MB (98.24%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,16 +1,17 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        def pow(x,n):
-            if n == 1:
-                return x
-            if n == 0:
-                return 1
+        # def pow(x,n):
+        #     if n == 1:
+        #         return x
+        #     if n == 0:
+        #         return 1
             
-            if n % 2 == 0:
-                return pow(x * x, n//2)
-            else:
-                return x * pow(x * x, (n-1)//2)
+        #     if n % 2 == 0:
+        #         return pow(x * x, n//2)
+        #     else:
+        #         return x * pow(x * x, (n-1)//2)
 
-        result = pow(x, abs(n))
+        # result = pow(x, abs(n))
 
-        return float(result) if n >= 1 else 1/result
+        # return float(result) if n >= 1 else 1/result
+        return x ** n
